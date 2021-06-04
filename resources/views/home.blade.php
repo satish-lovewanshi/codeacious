fb26844838a4b8770de07b5f4dd8e78cffc19277
satish-lovewanshi <satishlovewanshi1900@gmail.com>
2021-06-04 12:19:41 +0530

    Display User Profile Details After Login In user Dashboard page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.app')
+<style>
+    table,th,td{
+        border:1px solid black;
+        border-collapse:collapse;
+        padding:7px;
+    }
+</style>
 
 @section('content')
 <div class="container">
@@ -14,7 +21,40 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                   <h1>{{ ucfirst(Auth::user()->name) }} Your Profile Details</h1>
+                    <table style='width:100%'>
+                        <tr>
+                            <th>Name</th>
+                            <td>{{Auth::user()->name}}</td>
+                        </tr>
+                        <tr>
+                            <th>Phone No.</th>
+                            <td>{{Auth::user()->phone}}</td>
+                        </tr>
+                        <tr>
+                            <th>Address</th>
+                            <td>{{Auth::user()->address}}</td>
+                        </tr>
+                        <tr>
+                            <th>City</th>
+                            <td>{{Auth::user()->city}}</td>
+                        </tr>
+                        <tr>
+                            <th>State</th>
+                            <td>{{Auth::user()->state}}</td>
+                        </tr>
+                        <tr>
+                            <th>Country</th>
+                            <td>{{Auth::user()->country}}</td>
+                        </tr>
+                        <tr>
+                            <th>Zip</th>
+                            <td>{{Auth::user()->zip}}</td>
+                        </tr>
+                    
+                        
+                        
+                    </table>
                 </div>
             </div>
         </div>
